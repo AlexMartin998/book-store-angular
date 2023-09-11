a72aa295044c35b16e01071f2cb2f63a6ee8c93a
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-11 10:58:24 -0500

    [ADD]: initial admin layout design

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialModule } from '../material/material.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { BookListPageComponent } from './books/pages/book-list-page/book-list-page.component';
-import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { SaveBookPageComponent } from './books/pages/save-book-page/save-book-page.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
 @NgModule({
-  declarations: [BookListPageComponent, AdminLayoutComponent, SaveBookPageComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [
+    BookListPageComponent,
+    AdminLayoutComponent,
+    SaveBookPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class AdminModule {}
--- src/app/admin/books/pages/save-book-page/save-book-page.component.ts
+++ src/app/admin/books/pages/save-book-page/save-book-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-save-book-page',
+  selector: 'admin-save-book-page',
   templateUrl: './save-book-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SaveBookPageComponent {
+  public saveBookForm: FormGroup = this.fb.group({
+    title: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
+    ],
+    slug: ['', [Validators.required, Validators.pattern('[a-z0-9-]+')]],
+    description: ['', [Validators.required]],
+    price: ['', [Validators.required, Validators.min(0)]],
+    coverPath: ['', [Validators.required]],
+    filePath: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.saveBookForm.value);
+  }
 }
