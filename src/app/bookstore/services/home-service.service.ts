ecb0be741b43957f097608dc41f6308678ece0ba
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-10 19:16:03 -0500

    [ADD]: get books in HomeService

--- src/app/bookstore/services/home-service.service.ts
+++ src/app/bookstore/services/home-service.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BookPage } from '../shared/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getBooks(page: number, size: number): Observable<BookPage> {
+    const params = new HttpParams().set('page', page).set('size', size);
+
+    return this.http.get<BookPage>('http://localhost:3000/api/v1/home/books', {
+      params,
+    });
+  }
 }
