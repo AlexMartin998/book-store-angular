1180facce3474b3e87cea9d443b49619672d0ee6
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-12 17:09:05 -0500

    [ADD]: admin layout

--- src/app/admin/layout/admin-layout/admin-layout.component.ts
+++ src/app/admin/layout/admin-layout/admin-layout.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styles: [
-  ]
+  styleUrls: ['./admin-layout.component.css'],
 })
 export class AdminLayoutComponent {
-
+  public sidebarItems = [
+    { label: 'Books', icon: 'library_books', url: '/admin/books' },
+    { label: 'Users', icon: 'supervisor_account', url: './new' },
+  ];
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -32,6 +33,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTableModule,
     MatToolbarModule,
     MatTooltipModule,
+    MatListModule,
   ],
 })
 export class MaterialModule {}
