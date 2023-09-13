b9823873285eb8eb8923abd310d809d59c3a18be
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-12 21:35:47 -0500

    [ADD]: initial home page design

--- src/app/bookstore/pages/home-page/home-page.component.ts
+++ src/app/bookstore/pages/home-page/home-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HomeService } from '../../services/home.service';
+import { Book } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  private _books: Book[] = [];
 
+  constructor(private readonly homeService: HomeService) {}
+
+  ngOnInit(): void {
+    this.homeService.getLatestBooks().subscribe((books) => {
+      this._books = books;
+    });
+  }
+
+  get books() {
+    return this._books;
+  }
 }
