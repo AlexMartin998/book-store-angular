20176430bf6ba039b74269da6a66d5529238f929
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-12 21:35:02 -0500

    [ADD]: get latest books in home service

--- src/app/bookstore/services/home.service.ts
+++ src/app/bookstore/services/home.service.ts
@@ -1,20 +1,18 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { BookPage } from '../shared/interfaces';
+import { Book } from 'src/app/shared/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeService {
+  private readonly baseUrl: string = 'http://localhost:3000/api/v1';
+
   constructor(private http: HttpClient) {}
 
-  getBooks(page: number, size: number): Observable<BookPage> {
-    const params = new HttpParams().set('page', page).set('size', size);
-
-    return this.http.get<BookPage>('http://localhost:3000/api/v1/home/books', {
-      params,
-    });
+  getLatestBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.baseUrl}/home/books/latest-published`);
   }
 }
