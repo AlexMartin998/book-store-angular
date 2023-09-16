cb20db3e2a59376fe282557833284162bacbd65b
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-15 23:07:33 -0500

    [ADD]: find all orders by customer (auth user)

---
+++ src/app/dashboard/services/orders.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+import { PurchaseOrder } from 'src/app/shared/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  private readonly baseUrl: string = 'http://localhost:3000/api/v1';
+
+  private http = inject(HttpClient);
+
+  findOne(orderId: number): Observable<PurchaseOrder> {
+    return this.http
+      .get<PurchaseOrder>(`${this.baseUrl}/orders/${orderId}`)
+      .pipe(catchError((err) => throwError(() => err?.error?.message)));
+  }
+
+  findAllByCustomer(size: number = 10, page: number = 0): Observable<any> {
+    const params = new HttpParams().set('page', page).set('size', size);
+
+    return this.http.get(`${this.baseUrl}/orders`, { params });
+  }
+
+  downloadBookBasedOnOrderItem(
+    orderId: number,
+    itemId: number
+  ): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/orders/${orderId}/items/${itemId}/books/download`,
+      {
+        responseType: 'blob', // api returns a binary
+      }
+    );
+  }
+}
